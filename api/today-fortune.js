eaf3cd3317c079662dba33b0bca7cc53fd327129
a01026214133-eng <a01026214133@gmail.com>
2026-09-14 11:55:01 +0900

    Add files via upload

--- api/today-fortune.js
+++ api/today-fortune.js
@@ -1,4 +1,4 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
 
   console.log('KEY:', process.env.ANTHROPIC_API_KEY?.slice(0, 15));
@@ -31,4 +31,4 @@ module.exports = async function handler(req, res) {
     console.error(err);
     res.status(500).json({ error: err.message });
   }
-};
+}
